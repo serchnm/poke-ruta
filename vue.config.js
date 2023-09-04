f875ff719619381a951f0c2369037474c75914b8
Sergio N <snavarro@arkusnexus.com>
2023-09-04 12:11:00 -0700

    adding public path

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath : '/poke-ruta/',
+  publicPath: '/poke-ruta/',
   transpileDependencies: true,
   
 })
